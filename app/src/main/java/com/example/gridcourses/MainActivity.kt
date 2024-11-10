cbdbc248e54c7aa534c58939d9c72d670900a212
TheShamRio <xsham@inbox.ru>
2024-11-10 12:27:05 +0300

    Добавление изображения в TopicCard

--- app/src/main/java/com/example/gridcourses/MainActivity.kt
+++ app/src/main/java/com/example/gridcourses/MainActivity.kt
@@ -4,20 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gridcourses.data.DataSource
+import com.example.gridcourses.model.Topic
 import com.example.gridcourses.ui.theme.CoursesTheme
 
 
@@ -46,5 +54,21 @@ fun TopicGrid(modifier: Modifier = Modifier) {
         items(DataSource.topics) { topic ->
             TopicCard(topic)
         }
+    }
+}
+
+@Composable
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
+    Card {
+        Box {
+            Image(
+                painter = painterResource(id = topic.imageRes),
+                contentDescription = null,
+                modifier = modifier
+                    .size(width = 68.dp, height = 68.dp),
+                contentScale = ContentScale.Crop
+            )
+        }
+
     }
 }
